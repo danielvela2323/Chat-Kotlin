7d11455dd1e8f69e2200271ea612619082e12a6b
Daniel Vela <danieltecanvela88@gmail.com>
2026-07-29 12:12:12 -0600

    New things - fragments

---
+++ app/src/main/java/com/example/chat_kotlin/Fragmentos/FragmentChats.java
@@ -0,0 +1,23 @@
+package com.example.chat_kotlin.Fragmentos;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.chat_kotlin.R;
+
+
+public class FragmentChats extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chats2, container, false);
+    }
+}
--- app/src/main/java/com/example/chat_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/chat_kotlin/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.chat_kotlin
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.chat_kotlin.databinding.ActivityMainBinding
+
+//Queda pendiente cambiar el nombre de los fragments xml
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNV.setOnClickListener { item ->
+            when(item.id){
+                R.id.item_perfil->{
+                    //Visualizar el fragmento perfil
+                    true
+                }
+                R.id.item_usuarios->{
+                    // visualizar
+                    true
+                }
+                R.id.item_chats ->{
+                    true
+                }else->{
+                    false
+                }
+            }
         }
+
     }
 }
+
