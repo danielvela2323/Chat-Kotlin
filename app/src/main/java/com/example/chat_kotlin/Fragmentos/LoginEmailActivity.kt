82d7d7370d9d9e1eddb070c96299a3ba3a3acb71
Daniel Vela <danieltecanvela88@gmail.com>
2026-08-15 10:45:25 -0600

    Actualizar layout de login de text a hint

--- app/src/main/java/com/example/chat_kotlin/Fragmentos/LoginEmailActivity.kt
+++ app/src/main/java/com/example/chat_kotlin/Fragmentos/LoginEmailActivity.kt
@@ -6,16 +6,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.chat_kotlin.R
+import com.example.chat_kotlin.databinding.ActivityLoginEmailBinding
 
 class LoginEmailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginEmailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login_email)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginEmailBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        //Evento al hacer clic en "Registrase"
+        binding.tvRegistrarme.setOnClickListener {
+
         }
+
+
+
+
     }
 }
