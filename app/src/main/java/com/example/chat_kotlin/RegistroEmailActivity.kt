4893394ecb388bbee40e897e0a63f15581023763
Daniel Vela <danieltecanvela88@gmail.com>
2026-08-17 12:21:14 -0600

    Agregar funcion en archivo RegsitroEmail

--- app/src/main/java/com/example/chat_kotlin/RegistroEmailActivity.kt
+++ app/src/main/java/com/example/chat_kotlin/RegistroEmailActivity.kt
@@ -1,16 +1,51 @@
 package com.example.chat_kotlin
 
+import android.app.ProgressDialog
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.chat_kotlin.databinding.ActivityRegistroEmailBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegistroEmailActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityRegistroEmailBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro_email)
+        binding = ActivityRegistroEmailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Espere por favor")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        binding.btnRegistrar.setOnClickListener {
+            validarInformacion()
+        }
+
 
     }
+
+
+    private var nombres = ""
+    private var email = ""
+    private var password = ""
+    private var r_password = ""
+
+    private fun validarInformacion() {
+        nombres = binding.etNombres.text.toString().trim()
+        email = binding.etEmail.text.toString().trim()
+        password = binding.etPassword.text.toString().trim()
+        r_password = binding.etRPassword.text.toString().trim()
+    }
+
 }
+
+
 
