fae4c47aadc8c80d2137efb29dffcfaab76c2356
Daniel Vela <danieltecanvela88@gmail.com>
2026-08-22 00:17:06 -0600

    Arreglo de problema de paquetes Context e Intent en Fragment Perfil

--- app/src/main/java/com/example/chat_kotlin/Fragmentos/FragmentPerfil.kt
+++ app/src/main/java/com/example/chat_kotlin/Fragmentos/FragmentPerfil.kt
@@ -1,25 +1,24 @@
 package com.example.chat_kotlin.Fragmentos
 
 import android.content.Intent
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.chat_kotlin.OpcionesLoginActivity
-import com.example.chat_kotlin.R
-import com.example.chat_kotlin.databinding.ActivityLoginEmailBinding
 import com.example.chat_kotlin.databinding.FragmentPerfilBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.core.Context
+
 
 class FragmentPerfil : Fragment() {
 
     private lateinit var binding : FragmentPerfilBinding
-    private lateinit var mContext: Context
+    private lateinit var mContext : Context
     private lateinit var firebaseAuth: FirebaseAuth
 
-    override fun onAttach(context: android.content.Context) {
+    override fun onAttach(context: Context) {
         mContext = context
         super.onAttach(context)
     }
