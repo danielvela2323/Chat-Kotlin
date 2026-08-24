803b2f7cf84f3e2fc4d05bcfaa26fdfa2b803497
Daniel Vela <danieltecanvela88@gmail.com>
2026-08-23 21:18:38 -0600

    agregar funciones a LoginEmailActivity

--- app/src/main/java/com/example/chat_kotlin/LoginEmailActivity.kt
+++ app/src/main/java/com/example/chat_kotlin/LoginEmailActivity.kt
@@ -1,19 +1,34 @@
 package com.example.chat_kotlin
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import androidx.appcompat.app.AppCompatActivity
 import com.example.chat_kotlin.databinding.ActivityLoginEmailBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginEmailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginEmailBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginEmailBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Espere por favor")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+
+        binding.btnIngresar.setOnClickListener {
+            validarInformacion()
+        }
 
         //Evento al hacer clic en "Registrase"
         binding.tvRegistrarme.setOnClickListener {
@@ -21,6 +36,41 @@ class LoginEmailActivity : AppCompatActivity() {
         }
 
 
+
+
     }
+
+
+
+    private var email = ""
+    private var password = ""
+
+    private fun validarInformacion() {
+        email = binding.etEmail.text.toString().trim()
+        password = binding.etPassword.text.toString().trim()
+
+        if(email.isEmpty()){
+            binding.etEmail.error = "Ingrese email"
+            binding.etEmail.requestFocus()
+        }
+        else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.etEmail.error = "Email no valido"
+            binding.etEmail.requestFocus()
+        }
+        else if(password.isEmpty()){
+            binding.etPassword.error = "Ingrese password"
+            binding.etPassword.requestFocus()
+        }
+        else{
+            loguearUsuario()
+        }
+
+    }
+
+    private fun loguearUsuario() {
+        
+    }
+
+
 }
 
